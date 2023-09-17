563b5a0e237901365b4261bd0d6e1f813a4e7ce3
coolworld2049 <coolworld2049@gmail.com>
2023-09-17 03:34:37 +0300

    JSONResponse

--- src/userbot/routes/telegram/auth.py
+++ src/userbot/routes/telegram/auth.py
@@ -1,5 +1,10 @@
-from fastapi import APIRouter
+import json
+
+from fastapi import APIRouter, HTTPException
+from loguru import logger
+from starlette import status
 from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 import userbot
 from userbot import client
@@ -8,23 +13,35 @@ from userbot.schemas.auth import SendCodeRequest, SendConfirmationCodeRequest
 router = APIRouter(prefix="/telegram", tags=["telegram"])
 
 
-@router.post(
-    "/auth",
-)
+@router.post("/auth", response_model=dict)
 async def telegram_auth(request: Request, payload: SendCodeRequest):
+    """
+
+    :param request:
+    :param payload:
+    :return: sent_code: SentCode
+    """
     await client.connect()
-    if not await client.is_user_authorized():
-        await client.send_code_request(payload.phone)
-    return {"message": "Confirmation code sent"}
+    if await client.is_user_authorized():
+        user = await client.get_me()
+        return JSONResponse(json.loads(user.to_json(default=str)))
+    sent_code = await client.send_code_request(payload.phone)
+    return JSONResponse(json.loads(sent_code.to_json(default=str)))
 
 
-@router.post(
-    "/auth/sign_in",
-)
-async def telegram_auth(request: Request, payload: SendConfirmationCodeRequest):
+@router.post("/auth/sign_in", response_model=dict)
+async def telegram_sign_in(request: Request, payload: SendConfirmationCodeRequest):
+    """
+
+    :param request:
+    :param payload:
+    :return: user: User | SentCode
+    """
+    await client.connect()
     try:
-        await client.sign_in(payload.phone, payload.confirm_code)
+        user = await client.sign_in(payload.phone, payload.code)
         userbot.client = client
-        return {"message": "Success"}
-    except:
-        return {"message": "Failure"}
+        return JSONResponse(json.loads(user.to_json(default=str)))
+    except Exception as e:
+        logger.error(e)
+        return HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
