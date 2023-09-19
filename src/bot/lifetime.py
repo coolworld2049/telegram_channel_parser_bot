8c672b02da819554bfcf07851967aa87d4e5d68a
coolworld2049 <coolworld2049@gmail.com>
2023-09-19 21:24:16 +0300

    docker-compose.yml

--- src/bot/lifetime.py
+++ src/bot/lifetime.py
@@ -4,11 +4,9 @@ from loguru import logger
 from settings import get_settings
 from handlers import main
 from loader import bot
-from bot.userbot import userbot, userbot_connect  # noqa
 
 
 async def startup_bot(dp: Dispatcher) -> None:
-    # await userbot_connect()
     await bot.delete_my_commands()
     await bot.set_my_commands(get_settings().BOT_COMMANDS)
     dp.include_routers(main.router)
