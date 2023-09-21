cd1f9ed1d784c9fb4a178132e6b41b62e6d68576
coolworld2049 <coolworld2049@gmail.com>
2023-09-21 19:59:10 +0300

    min_subscribers

--- src/bot/cse/main.py
+++ src/bot/cse/main.py
@@ -44,8 +44,8 @@ async def selenium_remote_sessions(path="status"):
 async def telegram_parsing_handler(
     user: types.User, queries: list[str], limit=100, min_subscribers=1
 ):
-    search_results = []
-    filtered_search_results = []
+    search_results = set()
+    filtered_search_results = set()
 
     selenium_webdriver = webdriver.Remote(
         command_executor=get_settings().SE_WEBDRIVER_URL + "/wd/hub",
@@ -69,20 +69,21 @@ async def telegram_parsing_handler(
                 query,
                 limit,
             )
-            search_results.extend(lyzem_channels)
+            for lch in lyzem_channels:
+                search_results.add(lch)
             logger.info(
                 {
                     "index": i,
                     "query": query,
-                    "current_query_channels": len(set(lyzem_channels)),
-                    "unique_channels": len(set(search_results)),
+                    "channels": len(lyzem_channels),
+                    "all_channels": len(search_results),
                 }
             )
-        search_results = set(search_results)
         output = BufferedInputFile(
             "\n".join(list(search_results)).encode("utf-8"), "result.txt"
         )
-        await bot.send_document(user.id, output)
+        if output.data != b"":
+            await bot.send_document(user.id, output)
         m = "Check for channels existence"
         logger.info(m)
         await bot.send_message(user.id, "Check for channels existence")
@@ -96,36 +97,33 @@ async def telegram_parsing_handler(
             channel_exist = await check_channel_existence(
                 channel, 3, min_subscribers=min_subscribers
             )
-            if channel_exist and channel and channel not in filtered_search_results:
-                filtered_search_results.append(channel)
+            if channel_exist:
+                filtered_search_results.add(channel)
                 logger.info(f"{i}/{len(search_results)} channel {channel} ADDED")
             else:
                 logger.info(f"{i}/{len(search_results)} channel {channel} EXCLUDED")
             logger.info(
-                {"unique_filtered_search_results": len(filtered_search_results)}
+                f"unique_filtered_search_results: {len(filtered_search_results)}"
             )
     except Exception as e:
         logger.error(e)
     finally:
-        return set(filtered_search_results)
+        return filtered_search_results
 
 
 async def check_channel_existence(url, retries=3, *, min_subscribers=1):
-    new_url = url.replace("t.me/", "t.me/s/")
     headers = {
         "User-Agent": user_agent.random,
     }
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(new_url, headers=headers) as response:
+            async with session.get(url, headers=headers) as response:
                 response.raise_for_status()
                 html_content = await response.text()
                 soup = BeautifulSoup(html_content, "lxml")
-                link = soup.find("a", class_="tgme_header_link")
-                if link:
-                    subscribers_counter_str = link.find(
-                        "div", attrs={"tgme_header_counter"}
-                    ).text.split(" ")[0]
+                tgme_page_extra = soup.find("div", attrs="tgme_page_extra")
+                if tgme_page_extra:
+                    subscribers_counter_str = tgme_page_extra.text.split(" ")[0]
                     subscribers_counter = 0
                     if "K" not in subscribers_counter_str:
                         subscribers_counter = int(subscribers_counter_str)
@@ -133,7 +131,7 @@ async def check_channel_existence(url, retries=3, *, min_subscribers=1):
                         number = subscribers_counter_str.split("K")[0]
                         subscribers_counter = int(float(number) * 1000)
                     logger.info(
-                        f"url {new_url} subscribers_counter_str: {subscribers_counter_str}; subscribers_counter: {subscribers_counter}; min_subscribers: {min_subscribers}"
+                        f"url {url} subscribers_counter_str: {subscribers_counter_str}; subscribers_counter: {subscribers_counter}; min_subscribers: {min_subscribers}"
                     )
                     if subscribers_counter >= min_subscribers:
                         return True
@@ -142,8 +140,8 @@ async def check_channel_existence(url, retries=3, *, min_subscribers=1):
                 else:
                     return False
     except aiohttp.ClientError as e:
-        logger.error(f"url {new_url}. An error occurred: {e}")
+        logger.error(f"url {url}. An error occurred: {e}")
         if retries <= 0:
             return False
-        logger.info(f"url {new_url}. Retry")
+        logger.info(f"url {url}. Retry")
         await check_channel_existence(url, retries - 1, min_subscribers=min_subscribers)
