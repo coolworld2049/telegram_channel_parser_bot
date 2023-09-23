a8dcbd5ce0e1bb8fe0065b2c54fd5d0c5e0d2bfe
coolworld2049 <coolworld2049@gmail.com>
2023-09-23 09:30:45 +0300

    loader.py

--- src/bot/loader.py
+++ src/bot/loader.py
@@ -1,7 +1,5 @@
 from aiogram import Bot
-from fake_useragent import FakeUserAgent
 from redis.asyncio import Redis
-from selenium.webdriver.chrome.options import Options
 
 from settings import get_settings
 
