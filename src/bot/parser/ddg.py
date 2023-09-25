c8b1609f41b356a018367b4725176e99e11de04a
coolworld2049 <coolworld2049@gmail.com>
2023-09-25 14:25:08 +0300

    upd

--- src/bot/parser/ddg.py
+++ src/bot/parser/ddg.py
@@ -5,19 +5,21 @@ import aiohttp
 from aiogram import types
 from bs4 import BeautifulSoup
 from duckduckgo_search import DDGS
-from fake_useragent import FakeUserAgent, UserAgent
+from fake_useragent import UserAgent
 from loguru import logger
 from tqdm.contrib.telegram import tqdm
 
+from bot.parser.query_builder import get_code_by_region
 from bot.settings import get_settings
 
 
 async def filter_parsing_result(
     url: str,
-    retries: int = 3,
     min_subscribers: int = None,
+    retries: int = 3,
+    **kwargs,
 ):
-    user_agent = FakeUserAgent()
+    user_agent = UserAgent()
     headers = {
         "User-Agent": user_agent.random,
     }
@@ -34,7 +36,11 @@ async def filter_parsing_result(
             return False
         logger.info(f"{url}. Retry")
         await asyncio.sleep(abs(1 - retries) + 1)
-        await filter_parsing_result(url, retries - 1, min_subscribers)
+        await filter_parsing_result(
+            url,
+            min_subscribers=min_subscribers,
+            retries=retries - 1,
+        )
     soup = BeautifulSoup(source_html, "lxml")
     try:
         tgme_page_extra = soup.find("div", attrs="tgme_page_extra") or soup.find(
@@ -55,16 +61,13 @@ async def filter_parsing_result(
             else:
                 subs_str = subs_str.replace(" ", "")
                 subs_counter = int(subs_str)
-        # logger.debug(
-        #     {
-        #         "url": url,
-        #         "filter": {
-        #             "min_subscribers": {"lq": min_subscribers},
-        #             "subscribers": subs_counter,
-        #             "text": text,
-        #         },
-        #     },
-        # )
+        logger.debug(
+            {
+                "url": url,
+                "text": text,
+                "count": subs_counter,
+            },
+        )
         if subs_counter >= min_subscribers:
             return True
         else:
@@ -74,68 +77,66 @@ async def filter_parsing_result(
 
 
 async def ddg_parsing(
-    query: str,
+    query: tuple[str, str],
     retries: int = 5,
     timeout: float = 10,
+    search_limit=6,
     **kwargs,
 ):
-    search_result = []
+    dork = query[0]
+    q = query[1]
+    region_code = "wt-wt"
+    try:
+        if kwargs.get("region_name"):
+            region_code = get_code_by_region().get(kwargs.get("region_name"))
+    except:  # noqa
+        pass
+    rows: list[tuple] = []
     unique_result = set()
-    user_agent = UserAgent(min_percentage=1.1)
-
+    user_agent = UserAgent()
     headers = {"User-Agent": user_agent.random}
     try:
-        logger.info({"query": query})
         with DDGS(headers=headers, timeout=timeout) as ddgs:
-            await asyncio.sleep(0.75)
-            logger.debug("sleep 0.75")
-            for r in ddgs.text(query, safesearch="on", backend="api"):
-                search_result.append(r)
-        logger.info({"query": query, "search_result": len(search_result)})
-        for sr in search_result[:10]:
-            usp = urlsplit(sr["href"])
-            if usp.query:
-                usp = urlsplit(usp.geturl().replace(f"?{usp.query}", ""))
-            url = usp.geturl()
-            unique_result.add(url)
-        for i, unique_url in enumerate(unique_result.copy()):
-            filter_result = await filter_parsing_result(
-                unique_url,
-                **kwargs,
-            )
-            log_msg = {
-                "i": i,
-                "url": unique_url,
-                "filter": kwargs,
-                "filter_result": filter_result,
-            }
-            if not filter_result:
-                unique_result.discard(unique_url)
-                log_msg.update({"action": "DISCARD url from set"})
-                logger.debug(log_msg)
-            logger.debug(log_msg)
-        logger.info(
-            {
-                "query": query,
-                "search_result": len(search_result),
-                "unique_filtered_result": len(unique_result),
-            }
-        )
-        return {query: search_result}, unique_result
+            for i, r in enumerate(
+                ddgs.text(
+                    f"{dork} {q}",
+                    safesearch="moderate",
+                    backend="api",
+                    region=region_code,
+                )
+            ):
+                if i == search_limit:
+                    break
+                usp = urlsplit(r["href"])
+                if usp.query:
+                    usp = urlsplit(usp.geturl().replace(f"?{usp.query}", ""))
+                unique_result.add(usp.geturl())
+                rows.append(
+                    (
+                        q,
+                        dork,
+                        *list(r.values()),
+                    )
+                )
+        logger.info(f"Unique links number: {len(unique_result)}")
+        return rows, unique_result
     except Exception as e:
-        logger.error(f"{query}. Exception: {e}")
+        logger.error(f"query {query}. Exception: {e}")
         if retries <= 0:
             logger.exception(e)
             raise e
-        logger.info(f"{query}. Retry")
+        logger.info(f"query {query}. Retry")
         await asyncio.sleep(timeout)
         await ddg_parsing(query, retries - 1, timeout + 1, **kwargs)
 
 
 async def ddg_parsing_handler(
-    user: types.User, queries: list[str], min_subscribers: int
+    user: types.User,
+    queries: list[tuple[str, str]],
+    min_subscribers: int,
+    region_name: str,
 ):
-    raw_results = []
+    values = [["query", "dork", "title", "href", "body"]]
     results = set()
     for i, query in tqdm(  # noqa
         enumerate(queries),
@@ -143,25 +144,37 @@ async def ddg_parsing_handler(
         token=get_settings().BOT_TOKEN,
         chat_id=user.id,
     ):
-        logger.info(" ")
-        logger.info(f"index {i}. START")
+        logger.info(
+            f"INDEX {i}. Query `{query[1]}'. Region '{region_name}'. Min subscribers '{min_subscribers}'"
+        )
         unique_result = set()
         try:
-            raw_result, unique_result = await ddg_parsing(
-                query, min_subscribers=min_subscribers
+            rows, unique_result = await ddg_parsing(
+                query,
+                min_subscribers=min_subscribers,
+                region_name=region_name,
             )
-            raw_results.append(raw_result)
+            _unique_result = unique_result.copy()
+            for j, unique_url in enumerate(_unique_result):
+                filter_result = await filter_parsing_result(
+                    unique_url, min_subscribers=min_subscribers
+                )
+                log_msg = {
+                    "url": unique_url,
+                    "filter": {"min_subscribers": min_subscribers},
+                    "filter_result": filter_result,
+                }
+                if not filter_result:
+                    unique_result.discard(unique_url)
+                    log_msg.update({"action": "DISCARD"})
+                    logger.debug(log_msg)
+                else:
+                    log_msg.update({"action": "ADD"})
+                    logger.debug(log_msg)
             results = results.union(unique_result)
+            values.append(rows)
+            logger.info(f"Total unique links: {len(results)}")
         except Exception as e:
             logger.error(e)
-        logger.info(
-            {
-                "index": i,
-                "query": query,
-                "links": len(unique_result) if unique_result is not None else None,
-                "all_links": len(results),
-                "min_subscribers": min_subscribers,
-            }
-        )
-        logger.info(f"index {i}. END")
-    return results, raw_results
+
+    return results, values
